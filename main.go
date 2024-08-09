270ab00ffb438cde9be607e2fb2f50f7fcdb2b5c
ohno <ohno104dev@gmail.com>
2024-08-10 03:13:14 +0800

    feat: solved with mutex lock

--- main.go
+++ main.go
@@ -8,17 +8,20 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
 
 func main() {
 	msg = "Hello, world!"
+	var mux sync.Mutex
 
 	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	go updateMessage("Hello, universe!", &mux)
+	go updateMessage("Hello, cosmos!", &mux)
 	wg.Wait()
 
 	fmt.Println(msg)
